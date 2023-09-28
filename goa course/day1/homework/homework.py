c0deb7e934ac40bb4d033aecc38a7559f043e4a7
giorgobianibeka <146353638+giorgobianibeka@users.noreply.github.com>
2023-09-28 17:53:17 +0400

    Add files via upload

---
+++ goa course/day1/homework/homework.py
@@ -0,0 +1,25 @@
+score=100
+city="paris"
+age=56
+print(age)
+
+name="Tom"
+salary=75000
+print(name)
+
+player="James"
+score=55
+level=14
+
+movie="Titanic"
+year=1997
+print(movie)
+
+band="Beatles"
+song="Yesterday"
+song="Let it be"
+print(song)
+
+item=18
+boxes=6
+print(item/boxes)
