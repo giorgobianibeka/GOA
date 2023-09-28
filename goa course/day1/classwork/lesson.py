c61cd3f0425b55e6fffb60db605935168253ae4a
giorgobianibeka <146353638+giorgobianibeka@users.noreply.github.com>
2023-09-28 17:37:07 +0400

    Add files via upload

---
+++ goa course/day1/classwork/lesson.py
@@ -0,0 +1,10 @@
+# შექმენით თქვენი ოჯახის წევრების ასაკებისგან ცვლადები
+# დაპრინტეთ 5 წლის შემდეგ
+dad_age=52
+mom_age=51
+my_age=28
+operation=5
+print(dad_age +operation)
+print(mom_age+5)
+#mom_age=60 -variant1
+#mom_age+=5 -variant2
