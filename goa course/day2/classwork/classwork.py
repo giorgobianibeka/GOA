71251fc8ec4402c09deeff127f0432f2151c6399
giorgobianibeka <146353638+giorgobianibeka@users.noreply.github.com>
2023-09-30 13:00:02 +0400

    Add files via upload

---
+++ goa course/day2/classwork/classwork.py
@@ -0,0 +1,42 @@
+# message = "Debugging"
+# print(message)
+
+# print("Great Progress")
+
+# salary = 50000
+# role = "Analyst"
+# age = 29
+# print(salary)
+
+# # salary = 50000
+# # role = Analyst
+# # age -> 29
+# # print(salary)
+
+# #Declaring payment variables
+# currency = "USD"
+# amount = 200
+# status = "overdue"
+
+# #Displaying status
+# print(status)
+
+# salary = 900
+# new_salary = salary + 200
+# print(new_salary)
+
+# message = input()
+# print(message)
+
+# variable = 5/2
+# print(variable)
+
+# # operation with numbers
+# print(3 + 5)
+
+# # operation with strings
+# print("Iron" + "Man")
+
+a = "basket"
+b = "ball"
+print(a+b)
