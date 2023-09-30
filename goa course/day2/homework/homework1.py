8b139670350cc301d26f54c5a1ec29b802871150
giorgobianibeka <146353638+giorgobianibeka@users.noreply.github.com>
2023-09-30 13:00:46 +0400

    Add files via upload

---
+++ goa course/day2/homework/homework1.py
@@ -0,0 +1,9 @@
+name=input("your name ")
+surname=input("your surname ")
+age=input("your age ")
+print("your name is "+ name + " your surname is " + surname+" your age is " + age)
+
+your_age=int(input("your age "))
+new_age=your_age+25
+
+print("your age after 25 years later "+str(new_age))
