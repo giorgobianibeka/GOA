ec67fca4daef3c1c0f0b3383fa3701f28299b4c8
giorgobianibeka <146353638+giorgobianibeka@users.noreply.github.com>
2023-10-04 18:38:05 +0400

    Add files via upload

---
+++ goa course/day3/homework/homework.py
@@ -0,0 +1,13 @@
+#1
+age=int(input("enter your age: "))
+new_age=age*7/3+(4*9)
+print(new_age)
+#2
+price=7000
+new_price=price*62/100
+print("Fasi gaizarda " + str(new_price) + " larit")
+#3
+
+xinkali=input("ramdeni xinkali moitxova svanma? ")
+oficianti=input("ramdeni shetavaza oficianta? ")
+print("svani shedis saxinkleshi da "+ xinkali + " moitxovs "+xinkali+ " raa barem "+ oficianti + " aigeo eubneba oficianti" )
