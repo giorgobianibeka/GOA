6b0c42ff0c0be9db4c598b3b35c65acb46d37b7d
giorgobianibeka <146353638+giorgobianibeka@users.noreply.github.com>
2023-10-15 10:17:22 +0400

    Add files via upload

---
+++ goa course/day7/classwork/classwork.py
@@ -0,0 +1,8 @@
+# my_list=["xinkali","movedi","vashli"]
+# # print(my_list[])
+# # print(my_list[0],my_list[1],my_list[2])
+# my_sum=my_list[0]
+# print(my_sum)
+
+nums=[8,4,1,3,12,]
+print(nums[1:4]) #slising
---
+++ goa course/day7/homework/homework1.py
@@ -0,0 +1,71 @@
+#Pancakes
+#the list of breakfasts
+# breakfasts = [
+#   'Donuts', 'Waffles', 'Yogurt', 
+#   'Burrito', 'Toast']
+
+# #index of the item to be replaced
+# item = int(input())
+
+# #replace that item with "Pancakes"
+# breakfasts[item]="Pancakes"
+
+# #display the updated list
+# print(breakfasts)
+# vehicle = "airplane"
+# print(vehicle[0:3])
+# fruit = 'orange'
+# print(fruit[0:2])
+
+#Relay Race
+
+# players = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank"]
+
+# #Create 3 lists with 2 players each
+# #Use slicing to create a list for Group 1
+# g1 = players[0:2]
+
+# #Use slicing to create a list for Group 2
+# g2 = players[2:4]
+
+# #Use slicing to create a list for Group 3
+# g3 = players[4:6]
+
+# print("Group 1:")
+# #display the 1st group
+# print(g1)
+
+# print("Group 2:")
+# #display the 2nd group
+# print(g2)
+
+# print("Group 3:")
+# #display the 3rd group
+# print(g3)
+
+# animals =["cat", "dog", "bird", "cow"]
+# print(animals[-1]) # Last element
+# print(animals[-2]) # Second last element
+# print(animals[-3:]) # Last 3 elements
+# print(animals[-3:-1])
+
+# c = ['$', '£', '€', '¥']
+# print(c[1:-1])
+
+# c = ['$', '£', '€', '¥']
+# c[:2] = ['₣', '฿']
+# print(c)
+
+# vehicle = 'airplane'
+# vehicle[:3] = 'water'
+# print(vehicle)
+
+#step counts for the past week
+# steps = [1513, 5035, 7891, 1212, 2534, 4648, 3785]
+
+# #make a slice for the last 3 days
+# last_3 = steps[-3:]
+
+# #display the slice
+# print(last_3)
+
